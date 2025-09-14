eace8c6b5b45bee26d68e3e1da0723881c3e1e8d
abhisri073 <abhinavdilip8888@gmail.com>
2025-09-14 15:51:33 +0530

    project backend basic setup i.e. db,schema,controllers

---
+++ backend/models/company.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const companySchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+    },
+    website:{
+        type:String
+    },
+    location:{
+        type:String,
+    },
+    logo:{
+        type:String, //logo url
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true
+    }
+},{timestamp:true})
+
+export const Company=mongoose.model("Company",companySchema);
